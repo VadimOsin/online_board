e7e4bd1d5ff7fb053c78b5daab4ac931328b9746
VadimOsin <vadzim.osin2001@gmail.com>
2023-06-12 19:54:36 +0300

    fix build

--- front/src/modules/AdsById/AdsById.js
+++ front/src/modules/AdsById/AdsById.js
@@ -1,40 +1,114 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getOneByIdAds } from '../Board/axios/adsApi';
-import { Editor, EditorState } from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import React, { useState } from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Container,
+    Card,
+    CardContent,
+    CardMedia,
+    Box,
+    TextField,
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+    Divider,
+} from '@mui/material';
 
 const AdsById = () => {
-    const { id } = useParams();
-    const [ads, setAds] = useState();
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
 
-    // useEffect(() => {
-    //     getOneByIdAds(id)
-    //         .then(resp => setAds(resp))
-    //         .catch(e => console.log(e));
-    // }, []);
+    const news = {
+        id: 1,
+        title: 'Заголовок новости',
+        text: 'Текст новости',
+        image: 'https://phonoteka.org/uploads/posts/2021-05/1620315781_36-phonoteka_org-p-fon-novostei-pervogo-kanala-37.png',
+    };
 
-    const [editorState, setEditorState] = React.useState(
-        EditorState.createEmpty()
-    );
+    const handleCommentChange = (e) => {
+        setNewComment(e.target.value);
+    };
 
-    const editor = React.useRef(null);
+    const handleCommentSubmit = () => {
+        if (newComment.trim() !== '') {
+            const currentDate = new Date().toLocaleString();
+            setComments((prevComments) => [
+                ...prevComments,
+                {
+                    text: newComment,
+                    author: 'Имя Фамилия', // Замените на соответствующее имя и фамилию
+                    time: currentDate,
+                },
+            ]);
+            setNewComment('');
+        }
+    };
 
-    function focusEditor() {
-        editor.current.focus();
-    }
-
-    React.useEffect(() => {
-        focusEditor()
-    }, []);
     return (
-        <div onClick={focusEditor}>
-            <Editor
-                ref={editor}
-                editorState={editorState}
-                onChange={editorState => setEditorState(editorState)}
-            />
-        </div>
+        <Box>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Пользовательский заголовок
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="md" sx={{ marginTop: 4 }}>
+                <Card>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={news.image}
+                        alt={news.title}
+                    />
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {news.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {news.text}
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Box sx={{ marginTop: 4 }}>
+                    <Typography variant="h6" gutterBottom>
+                        Комментарии
+                    </Typography>
+                    <List>
+                        {comments.map((comment, index) => (
+                            <React.Fragment key={index}>
+                                <ListItem>
+                                    <ListItemText
+                                        primary={`${comment.author} - ${comment.time}`}
+                                        secondary={comment.text}
+                                    />
+                                </ListItem>
+                                <Divider />
+                            </React.Fragment>
+                        ))}
+                    </List>
+                    <Box sx={{ marginTop: 2 }}>
+                        <TextField
+                            label="Оставить комментарий"
+                            variant="outlined"
+                            fullWidth
+                            value={newComment}
+                            onChange={handleCommentChange}
+                        />
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleCommentSubmit}
+                            sx={{ marginTop: 2 }}
+                        >
+                            Отправить
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </Box>
     );
 };
 
