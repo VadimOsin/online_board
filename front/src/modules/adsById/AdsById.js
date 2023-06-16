36d658e6ed072a91dd9dc6fb61f3ecf62ff19184
VadimOsin <vadzim.osin2001@gmail.com>
2023-06-16 14:43:04 +0300

    fix likes and dislike and add function new comment

--- front/src/modules/adsById/AdsById.js
+++ front/src/modules/adsById/AdsById.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
     Typography,
     Container,
@@ -11,25 +11,30 @@ import {
     List,
     ListItem,
     ListItemText,
-    Divider, Paper,
+    Divider,
+    Paper,
 } from '@mui/material';
-import {getOneByIdAds} from "../board/axios/adsApi";
-import {useParams} from "react-router-dom";
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import {getAllComments} from "./axios/comments";
-import {getUserInfo} from "./axios/user";
+import { getOneByIdAds } from '../board/axios/adsApi';
+import { getAllComments, addComment } from './axios/comments';
+import { getUserInfo } from './axios/user';
+import { UserContext } from '../auth/context/userContext';
+import {useParams} from "react-router-dom";
 
 const AdsById = () => {
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
-    const {id} = useParams();
+    const { id } = useParams();
     const [news, setNews] = useState(null);
+    const user = useContext(UserContext);
+    const [liked, setLiked] = useState(false);
+    const [disliked, setDisliked] = useState(false);
 
     useEffect(() => {
         getOneByIdAds(id)
-            .then(res => setNews(res))
-            .catch(e => console.log(e.data.message));
+            .then((res) => setNews(res))
+            .catch((e) => console.log(e.data.message));
     }, [id]);
 
     useEffect(() => {
@@ -39,7 +44,7 @@ const AdsById = () => {
                 const commentsWithUsers = await Promise.all(
                     commentsData.map(async (comment) => {
                         const user = await getUserInfo(comment.id_person);
-                        return {...comment, user};
+                        return { ...comment, user };
                     })
                 );
                 setComments(commentsWithUsers);
@@ -55,31 +60,64 @@ const AdsById = () => {
         setNewComment(e.target.value);
     };
 
-    const handleCommentSubmit = () => {
+    const handleCommentSubmit = async () => {
         if (newComment.trim() !== '') {
-            const currentDate = new Date().toLocaleString();
-            setComments((prevComments) => [
-                ...prevComments,
-                {
-                    text: newComment,
-                    author: 'Имя Фамилия', // Замените на соответствующее имя и фамилию
-                    time: currentDate,
+            const currentDate = new Date().toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            });
+            const newCommentData = {
+                text: newComment,
+                user: {
+                    login: user.login,
                 },
-            ]);
-            setNewComment('');
+                date_created: currentDate,
+            };
+            try {
+                // await addComment(id, newCommentData);
+                setComments((prevComments) => [...prevComments, newCommentData]);
+                setNewComment('');
+            } catch (error) {
+                console.log(error);
+            }
         }
     };
 
     const handleLike = () => {
-        // Обработчик для лайков
+        if (!liked && !disliked) {
+            setLiked(true);
+            setNews((prevNews) => ({
+                ...prevNews,
+                likes: prevNews.likes + 1,
+            }));
+        } else if (liked && !disliked) {
+            setLiked(false);
+            setNews((prevNews) => ({
+                ...prevNews,
+                likes: prevNews.likes - 1,
+            }));
+        }
     };
 
     const handleDislike = () => {
-        // Обработчик для дизлайков
+        if (!liked && !disliked) {
+            setDisliked(true);
+            setNews((prevNews) => ({
+                ...prevNews,
+                dislikes: prevNews.dislikes + 1,
+            }));
+        } else if (!liked && disliked) {
+            setDisliked(false);
+            setNews((prevNews) => ({
+                ...prevNews,
+                dislikes: prevNews.dislikes - 1,
+            }));
+        }
     };
 
     const formatDate = (dateString) => {
-        const options = {year: 'numeric', month: 'long', day: 'numeric'};
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const date = new Date(dateString);
         return date.toLocaleDateString('en-US', options);
     };
@@ -88,7 +126,7 @@ const AdsById = () => {
         <>
             {news ? (
                 <Box>
-                    <Container maxWidth="md" sx={{marginTop: 4}}>
+                    <Container maxWidth="md" sx={{ marginTop: 4 }}>
                         <Card>
                             <CardMedia
                                 component="img"
@@ -106,11 +144,11 @@ const AdsById = () => {
                             </CardContent>
                             <CardContent>
                                 <Box display="flex" alignItems="center">
-                                    <Button startIcon={<ThumbUpIcon/>} onClick={handleLike}>
+                                    <Button startIcon={<ThumbUpIcon />} onClick={handleLike} disabled={liked}>
                                         {news.likes}
                                     </Button>
-                                    <Button startIcon={<ThumbDownIcon/>} onClick={handleDislike}>
-                                        {news.dislike}
+                                    <Button startIcon={<ThumbDownIcon />} onClick={handleDislike} disabled={disliked}>
+                                        {news.dislikes}
                                     </Button>
                                 </Box>
                                 <Typography variant="body2" color="text.secondary">
@@ -118,38 +156,28 @@ const AdsById = () => {
                                 </Typography>
                             </CardContent>
                         </Card>
-                        <Box sx={{marginTop: 4}}>
+                        <Box sx={{ marginTop: 4 }}>
                             <Typography variant="h6" gutterBottom>
                                 Комментарии
                             </Typography>
-                            <Paper elevation={3} style={{padding: '16px', marginTop: '16px', border: '1px solid #ccc'}}>
+                            <Paper elevation={3} style={{ padding: '16px', marginTop: '16px', border: '1px solid #ccc' }}>
                                 <List>
                                     {comments.map((comment, index) => (
-                                        <React.Fragment key={comment.id_comments}>
-                                            <Paper elevation={3} style={{
-                                                padding: '16px',
-                                                marginTop: '16px',
-                                                border: '1px solid #ccc'
-                                            }}>
+                                        <React.Fragment key={comment?.id_comments}>
+                                            <Paper elevation={3} style={{ padding: '16px', marginTop: '16px', border: '1px solid #ccc' }}>
                                                 <ListItem>
-                                                    <ListItemText
-                                                        primary={comment.title}
-                                                        secondary={comment.text}
-                                                    />
+                                                    <ListItemText secondary={comment.text} />
                                                 </ListItem>
                                                 <ListItem>
-                                                    <ListItemText
-                                                        primary={`${comment?.user?.name} ${comment?.user?.surname}`}
-                                                        secondary={formatDate(comment.date_created)}
-                                                    />
+                                                    <ListItemText primary={`${comment?.user?.login}`} secondary={formatDate(comment.date_created)} />
                                                 </ListItem>
                                             </Paper>
-                                            {index !== comments.length - 1 && <Divider/>}
+                                            {index !== comments.length - 1 && <Divider />}
                                         </React.Fragment>
                                     ))}
                                 </List>
                             </Paper>
-                            <Box sx={{marginTop: 2}}>
+                            <Box sx={{ marginTop: 2 }}>
                                 <TextField
                                     label="Оставить комментарий"
                                     variant="outlined"
@@ -157,12 +185,7 @@ const AdsById = () => {
                                     value={newComment}
                                     onChange={handleCommentChange}
                                 />
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={handleCommentSubmit}
-                                    sx={{marginTop: 2}}
-                                >
+                                <Button variant="contained" color="primary" onClick={handleCommentSubmit} sx={{ marginTop: 2 }}>
                                     Отправить
                                 </Button>
                             </Box>
